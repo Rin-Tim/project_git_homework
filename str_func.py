89a97a9702af346853be73c03ec7fb2a51c74a38
Rinat Timoshenko <rintim21v@gmail.com>
2024-03-29 16:53:28 +0300

    add 2 function in str_func.py and docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,16 @@
 def str_func_upper():
+    """Функция возвращения строк со всеми заглавными буквами"""
     us_input = input("Впиши - ").upper()
     return us_input
 
 
 print(str_func_upper())
+
+
+def str_func_title():
+    """функция делает заглавными первые буквы каждого слова в строке"""
+    us_input = input("Впиши - ").title()
+    return us_input
+
+
+print(str_func_title())
